12155ca2766ffa224ede0ef1bed4067aa8f5dc49
Telmen <telmenganhuyag@gmail.com>
2024-08-29 14:32:33 +0800

    continue

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom'
+import HomePage from './pages/HomePage';
+import MainLayout from './pages/MainLayout';
+import JobsPage from './pages/JobsPage';
+import NotFoundPage from './pages/NotFoundPage';
+import JobPage, { jobLoader } from './pages/JobPage';
+import AddJobPage from './pages/AddJobPage';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+  <Route path='/' element={<MainLayout />}>
+    <Route index element={<HomePage />} />
+    <Route path='/jobs' element={<JobsPage />} />
+    <Route path='/add-job' element={<AddJobPage />} />
+    <Route path='/jobs/:id' element={<JobPage />} loader={jobLoader} />
+    <Route path='*' element={<NotFoundPage />} />
+  </Route>
+  )
+);
+
+const App = () => {
+  return <RouterProvider router={router} />;
+};
+
+export default App;
